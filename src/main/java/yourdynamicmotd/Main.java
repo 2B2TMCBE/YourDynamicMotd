ac48591a00e368cac39e10f1ce964241803ac25c
maxxie114 <maxxie114@gmail.com>
2019-04-04 03:01:52 -0700

    Fix NullPointerException
    close #1

--- src/main/java/yourdynamicmotd/Main.java
+++ src/main/java/yourdynamicmotd/Main.java
@@ -1,56 +1,65 @@
 package yourdynamicmotd;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
 import cn.nukkit.plugin.PluginBase;
 import cn.nukkit.scheduler.Task;
 import cn.nukkit.utils.Config;
 import cn.nukkit.utils.TextFormat;
 import yourdynamicmotd.task.MotdTask;
 
-import java.io.File;
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.List;
+public class Main extends PluginBase {
+  public List<String> list;
+  public Config config;
 
-public class Main
-        extends PluginBase {
-    public List<String> list;
-    public Config config;
-
-    public void onEnable() {
-        this.getLogger().info(TextFormat.colorize((String) "&aDynamic motd loaded :D"));
-        if (!this.getDataFolder().exists()) {
-            this.getDataFolder().mkdirs();
-            this.config = new Config(new File(this.getDataFolder() + "/config.yml"), 2, (LinkedHashMap<String, Object>) new LinkedHashMap<String, Object>() {
+  public void onEnable() {
+    this.getLogger().info(TextFormat.colorize((String) "&aDynamic motd loaded :D"));
+    if (!this.getDataFolder().exists()) {
+      this.getDataFolder().mkdirs();
+      this.config = new Config(new File(this.getDataFolder() + "/config.yml"), 2,
+          (LinkedHashMap<String, Object>) new LinkedHashMap<String, Object>() {
+            {
+              this.put("motd-type", new ArrayList<String>() {
                 {
-                    this.put("motd-type", new ArrayList<String>() {
-                        {
-                            this.add("Your dynamic motd 1");
-                            this.add("Your dynamic motd 2");
-                            this.add("Your dynamic motd 3");
-                        }
-                    });
-                    this.put("time-update", 3);
+                  this.add("Your dynamic motd 1");
+                  this.add("Your dynamic motd 2");
+                  this.add("Your dynamic motd 3");
                 }
+              });
+              this.put("time-update", 3);
+            }
 
-            });
-        } else {
-            this.config = new Config(new File(this.getDataFolder() + "/config.yml"), 2, (LinkedHashMap<String, Object>) new LinkedHashMap<String, Object>() {
+          });
+    } else {
+      this.config = new Config(new File(this.getDataFolder() + "/config.yml"), 2,
+          (LinkedHashMap<String, Object>) new LinkedHashMap<String, Object>() {
+            {
+              this.put("motd-type", new ArrayList<String>() {
                 {
-                    this.put("motd-type", new ArrayList<String>() {
-                        {
-                            this.add("Your dynamic motd 1");
-                            this.add("Your dynamic motd 2");
-                            this.add("Your dynamic motd 3");
-                        }
-                    });
-                    this.put("time-update", 3);
+                  this.add("Your dynamic motd 1");
+                  this.add("Your dynamic motd 2");
+                  this.add("Your dynamic motd 3");
                 }
+              });
+              this.put("time-update", 3);
+            }
 
-            });
-        }
-        int time = (Integer) this.config.getNestedAs("time-update", Integer.TYPE);
-        this.getServer().getScheduler().scheduleRepeatingTask((Task) new MotdTask(this), time * 20);
+          });
     }
+    int time = (Integer) this.config.getNestedAs("time-update", Integer.TYPE);
+    this.getServer().getScheduler().scheduleRepeatingTask((Task) new MotdTask(this), time * 20);
+  }
+
+  /**
+   * This plugin will change the server motd
+   * 
+   * @param String motd
+   */
+  public void setMotd(String motd) {
+    this.getServer().getNetwork().setName(motd);
+  }
 
 }
 
--- src/main/java/yourdynamicmotd/task/MotdTask.java
+++ src/main/java/yourdynamicmotd/task/MotdTask.java
@@ -1,24 +1,27 @@
 package yourdynamicmotd.task;
 
-import cn.nukkit.scheduler.PluginTask;
-import cn.nukkit.utils.TextFormat;
-import yourdynamicmotd.Main;
-
 import java.util.ArrayList;
 import java.util.Random;
+import cn.nukkit.scheduler.PluginTask;
+import yourdynamicmotd.Main;
 
-public class MotdTask
-        extends PluginTask<Main> {
-    public MotdTask(Main owner) {
-        super((Main) owner);
-    }
+public class MotdTask extends PluginTask<Main> {
 
-    public void onRun(int ticks) {
-        ArrayList name = (ArrayList) ((Main) this.getOwner()).config.get("motd-type");
-        int max = name.size();
-        Random rand = new Random();
-        int motd = rand.nextInt(max);
-        ((Main) this.getOwner()).getServer().getNetwork().setName(TextFormat.colorize((String) ((String) name.get(motd)).toString()));
+  public MotdTask(Main owner) {
+    super(owner);
+  }
+
+  public void onRun(int ticks) {
+    ArrayList<String> name = new ArrayList<String>();
+    name = (ArrayList<String>) ((Main) this.getOwner()).config.get("motd-type");
+    if (name == null) {
+      name.add("This is a nukkit server.");
     }
+    int max = name.size();
+    Random rand = new Random();
+    int motd = rand.nextInt(max);
+    this.owner.setMotd(name.get(motd));
+    this.owner.getLogger().debug(this.owner.getServer().getNetwork().getName()); // debug
+  }
 }
 
